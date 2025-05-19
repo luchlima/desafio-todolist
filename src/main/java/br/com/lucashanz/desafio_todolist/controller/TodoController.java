18a278b73370dc0c71cded7acdccb10a2e982b3e
luchlima <lucashlima78@gmail.com>
2025-05-19 20:10:35 -0300

    Controlador criado e endpoints gerados para cada método.

---
+++ src/main/java/br/com/lucashanz/desafio_todolist/controller/TodoController.java
@@ -0,0 +1,37 @@
+package br.com.lucashanz.desafio_todolist.controller;
+
+import br.com.lucashanz.desafio_todolist.entity.Todo;
+import br.com.lucashanz.desafio_todolist.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo){
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list(){
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo){
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id){
+        return todoService.delete(id);
+    }
+}
